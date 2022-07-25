773113ba1e26bbc3d8f9b27c86102aa5668dcf47
Sheep <51675514+sheeperino@users.noreply.github.com>
2022-07-25 13:49:20 +0200

    Fixed symlink

--- Startup/komorebi_setup.py
+++ Startup/komorebi_setup.py
@@ -1,1 +1,21 @@
-komorebi_setup.py
+import ctypes
+import requests
+import subprocess
+
+# version check :3
+url = 'https://github.com/LGUG2Z/komorebi/releases/latest'
+r = requests.get(url)
+gh_ver = r.url.split('/')[-1].replace('v','')
+
+output = subprocess.run(['komorebic', '--version'],
+            stdout=subprocess.PIPE).stdout.decode('utf-8')
+cmd_ver = output.split()[-1]
+
+if cmd_ver != gh_ver:
+    ctypes.windll.user32.MessageBoxW(
+            0, "A new komorebi version is available!", "annoying box", 32)
+
+
+# https://github.com/LGUG2Z/komorebi#running 
+subprocess.call(['komorebic', 'start'])
+
