ce48e70f5380fa95040aba6af59d8dd4a144976a
Sheep <51675514+sheeperino@users.noreply.github.com>
2022-08-12 13:34:40 +0200

    Scoop-only update checker

--- Startup/komorebi_setup.py
+++ Startup/komorebi_setup.py
@@ -1,21 +1,15 @@
 import ctypes
-import requests
 import subprocess
 
 # version check :3
-url = 'https://github.com/LGUG2Z/komorebi/releases/latest'
-r = requests.get(url)
-gh_ver = r.url.split('/')[-1].replace('v','')
+output = subprocess.check_output('scoop info komorebi', text=True, shell=True)
 
-output = subprocess.run(['komorebic', '--version'],
-            stdout=subprocess.PIPE).stdout.decode('utf-8')
-cmd_ver = output.split()[-1]
+update_available = 'Update to' in output
 
-if cmd_ver != gh_ver:
-    ctypes.windll.user32.MessageBoxW(
-            0, "A new komorebi version is available!", "annoying box", 32)
-
+if update_available:
+    msg = "A new komorebi version is available!"
+    title = "annoying box"
+    ctypes.windll.user32.MessageBoxW(0, msg, title, 32)
 
 # https://github.com/LGUG2Z/komorebi#running 
-subprocess.call(['komorebic', 'start'])
-
+subprocess.run('komorebic start', text=True)
