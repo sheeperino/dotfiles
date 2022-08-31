f9cd12ad10ccc4647de03069dc294339559337ef
Sheep <51675514+sheeperino@users.noreply.github.com>
2022-08-31 14:45:19 +0200

    Added scoop update

--- Startup/komorebi_setup.py
+++ Startup/komorebi_setup.py
@@ -1,7 +1,11 @@
 import ctypes
 import subprocess
 
+# https://github.com/LGUG2Z/komorebi#running 
+subprocess.run('komorebic start --await-configuration', text=True)
+
 # version check :3
+subprocess.run('scoop update', text=True, shell=True) # check for scoop buckets update first
 output = subprocess.check_output('scoop info komorebi', text=True, shell=True)
 
 update_available = 'Update to' in output
@@ -10,6 +14,3 @@ if update_available:
     msg = "A new komorebi version is available!"
     title = "annoying box"
     ctypes.windll.user32.MessageBoxW(0, msg, title, 32)
-
-# https://github.com/LGUG2Z/komorebi#running 
-subprocess.run('komorebic start --await-configuration', text=True)
