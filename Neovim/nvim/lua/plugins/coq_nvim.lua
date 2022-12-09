13a49065a49458b4f93dedebb007a276e160b200
sheeperino <sheeperinosophie@gmail.com>
2022-12-09 13:03:54 +0100

    More coq changes

--- Neovim/nvim/lua/plugins/coq_nvim.lua
+++ Neovim/nvim/lua/plugins/coq_nvim.lua
@@ -1,5 +1,8 @@
 vim.g.coq_settings = {
+  ['xdg'] = true,
   ['display.pum.fast_close'] = false,
+  ['display.preview.border'] = "single",
+  ['display.ghost_text.context'] = { " ", " " },
   ['auto_start'] = 'shut-up',
   ['keymap.jump_to_mark'] = '<C-e>',
 }
