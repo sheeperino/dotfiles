6708c80a1719b7c8162a289bf33266d3f229e495
sheeperino <sheeperinosophie@gmail.com>
2022-12-14 13:18:21 +0100

    try chadtree

---
+++ Neovim/nvim/lua/plugins/chadtree.lua
@@ -0,0 +1,14 @@
+vim.g.chadtree_settings = {
+  ['options.close_on_open'] = true,
+  ['view.open_direction'] = "right",
+  ['view.width'] = 30,
+  ['theme.text_colour_set'] = "nord",
+  ['keymap.primary'] = {"l"},
+  ['keymap.copy_name'] = {"yn"},
+  ['keymap.select'] = {"m"},
+  ['keymap.clear_selection'] = {"M"},
+  ['keymap.copy'] = {"p"}, -- this actually pastes things
+  ['keymap.change_focus_up'] = {"h"},
+  ['keymap.change_focus'] = {"s"},
+  ['keymap.delete'] = {"dd"}, -- i don't trust myself
+}
