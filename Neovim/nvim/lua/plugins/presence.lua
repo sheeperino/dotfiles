1fbe30350596844778cf6c8500a2d267286827e6
sheeperino <sheeperinosophie@gmail.com>
2022-12-14 13:19:09 +0100

    Fix presence.nvim to work on WSL(2)

--- Neovim/nvim/lua/plugins/presence.lua
+++ Neovim/nvim/lua/plugins/presence.lua
@@ -1,6 +1,11 @@
 -- use ":" ig???
 require('presence'):setup({
-  neovim_image_text = 'Neovide',
+  neovim_image_text = 'Neovim',
   main_image = 'file',
   enable_line_number = true
 })
+
+-- check https://github.com/andweeb/presence.nvim/wiki/Rich-Presence-in-WSL#wsl-1
+-- to setup presence on WSL (it works on WSL2 too)
+-- then add the following line to the last line of /etc/sudoers RUN WITH sudo visudo !!
+-- <sheepsophie ALL=(root) NOPASSWD: /path/to/discord-relay.sh>
