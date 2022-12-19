58f66195ef4ca98e64c2afa096f788236fd8a875
sheeperino <sheeperinosophie@gmail.com>
2022-12-19 11:30:02 +0100

    Add hydro prompt

--- Fish/functions/fish_prompt_loading_indicator.fish
+++ Fish/functions/fish_prompt_loading_indicator.fish
@@ -1,4 +1,4 @@
-function fish_prompt_loading_indicator --argument last_prompt
+function fish_right_prompt_loading_indicator -a last_prompt
     echo -n "$last_prompt" | sed -r 's/\x1B\[[0-9;]*[JKmsu]//g' | read -zl uncolored_last_prompt
     echo -n (set_color brblack)"$uncolored_last_prompt"(set_color normal)
 end
